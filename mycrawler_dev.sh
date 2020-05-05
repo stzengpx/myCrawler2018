93ac82b82022f70727ade8a29d52421af22aa56c
stzengpx <st.zengpx@gmail.com>
2020-05-05 11:31:54 +0800

    chore(mycrawler_dev.sh): modify cp mycrawler_dev

--- mycrawler_dev.sh
+++ mycrawler_dev.sh
@@ -7,8 +7,8 @@ if [[ $1 == "update" ]];then
     rm chinapost.zi*
     wget https://github.com/stzengpx/myCrawler2018/archive/chinapost.zip
     unzip -oq chinapost.zip
-    cp myCrawler2018-chinapost/mycrawler.sh ~/findbiz/
-    cp myCrawler2018-chinapost/mycrawlerrun.sh ~/findbiz/
+    cp myCrawler2018-chinapost/mycrawler_dev.sh ~/findbiz/
+    cp myCrawler2018-chinapost/mycrawlerrun_dev.sh ~/findbiz/
     echo "Update Complete!"
 fi
 rm chinapost.zi*
